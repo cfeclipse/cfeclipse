d04cabb795626b60a1d939c6b2638947cc2fefca
Rob Rohan <me@robrohan.com>
2004-05-09 06:00:41 +0000

    The non-working content ouline - this is also *not* the proper outline eclipse expects. The adapter class that the doc defines as the proper class to subclass *doesnt exist in 3.0*!

---
+++ cvs/trunk/src/version1/src/com/rohanclan/cfml/views/contentoutline/CFContentOutlineView.java
@@ -0,0 +1,195 @@
+/*
+ * Created on Apr 6, 2004
+ *
+ * TODO To change the template for this generated file go to
+ * Window - Preferences - Java - Code Generation - Code and Comments
+ */
+package com.rohanclan.cfml.views.contentoutline;
+
+//import org.eclipse.jface.viewers.TreeViewer;
+//import org.eclipse.swt.widgets.Control;
+
+/* import org.eclipse.jface.action.Action;
+import org.eclipse.jface.action.IMenuListener;
+import org.eclipse.jface.action.IMenuManager;
+import org.eclipse.jface.action.IToolBarManager;
+import org.eclipse.jface.action.MenuManager;
+import org.eclipse.jface.viewers.ISelectionChangedListener;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.viewers.SelectionChangedEvent; */
+import org.eclipse.jface.viewers.TreeViewer;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Text;
+import org.eclipse.ui.part.ViewPart;
+import org.eclipse.jface.viewers.LabelProvider;
+/* import org.eclipse.jface.action.GroupMarker;
+import org.eclipse.swt.widgets.Menu;
+import org.eclipse.ui.IWorkbenchActionConstants;
+import org.eclipse.ui.IEditorPart;
+import com.rohanclan.cfml.util.XMLConfigFile;
+import org.eclipse.core.runtime.IPath;
+import com.rohanclan.cfml.CFMLPlugin;
+import com.rohanclan.cfml.editors.actions.GenericEncloserAction;
+import java.io.File;
+*/
+
+import org.eclipse.ui.IEditorPart;
+import org.eclipse.ui.part.FileEditorInput;
+import org.eclipse.ui.texteditor.ITextEditor;
+import com.rohanclan.cfml.editors.ICFDocument;
+import com.rohanclan.cfml.parser.*;
+import java.util.*;
+
+/**
+ * @author Rob
+ *
+ * TODO To change the template for this generated type comment go to
+ * Window - Preferences - Java - Code Generation - Code and Comments
+ */
+public class CFContentOutlineView extends ViewPart {
+	public static final String ID_SNIPVIEWTREE = "com.rohanclan.cfml.views.contentoutline.cfcontentoutlineview";
+	
+	/** the treeviewer control */
+	protected TreeViewer treeViewer;
+	protected Text text;
+	protected LabelProvider labelProvider;
+		
+	/** the path to the icons. i.e. file://C/blah/plugin/icons/ */
+	//protected static IPath snipBase;
+	/** used as a proxy action to add snips to the editor */
+	//private static GenericEncloserAction tmpAction;
+	/** Config file is used to load simple xml documents and get to
+	 * simple items via DOM - not recommended for large documents
+	 */
+	//private static XMLConfigFile xmlconfile;
+	
+	//protected Action insertAction; //addItemAction; //, deleteItemAction, selectAllAction;
+	
+	/** the root directory */
+	//protected File root;
+	
+	public void createPartControl(Composite parent) 
+	{
+		//Create a grid layout object so the text and treeviewer
+		GridLayout layout = new GridLayout();
+		layout.numColumns = 1;
+		layout.verticalSpacing = 2;
+		layout.marginWidth = 0;
+		layout.marginHeight = 2;
+		parent.setLayout(layout);
+		
+		//Create a "label" to display information in. I'm
+		//using a text field instead of a lable so you can
+		//copy-paste out of it.
+		text = new Text(parent, SWT.READ_ONLY | SWT.SINGLE | SWT.BORDER);
+		// layout the text field above the treeviewer
+		GridData layoutData = new GridData();
+		layoutData.grabExcessHorizontalSpace = true;
+		layoutData.horizontalAlignment = GridData.FILL;
+		text.setLayoutData(layoutData);
+		
+		//Create the tree viewer as a child of the composite parent
+		treeViewer = new TreeViewer(parent);
+		treeViewer.setContentProvider(
+			new OutlineContentProvider(getRootInput())
+		);
+		labelProvider = new OutlineLabelProvider();
+		treeViewer.setLabelProvider(labelProvider);
+		
+		treeViewer.setUseHashlookup(true);
+		
+		//layout the tree viewer below the text field
+		layoutData = new GridData();
+		layoutData.grabExcessHorizontalSpace = true;
+		layoutData.grabExcessVerticalSpace = true;
+		layoutData.horizontalAlignment = GridData.FILL;
+		layoutData.verticalAlignment = GridData.FILL;
+		treeViewer.getControl().setLayoutData(layoutData);
+		
+		//Create menu, toolbars, filters
+		createActions();
+		createMenus();
+		createToolbar();
+		createContextMenu();
+		hookListeners();
+		
+		treeViewer.setInput(getRootInput());
+		//treeViewer.expandAll();
+	}
+	
+	protected void hookListeners(){;}
+	protected void createActions(){;}
+	protected void createMenus(){
+		//IMenuManager rootMenuManager = getViewSite().getActionBars().getMenuManager();
+		//rootMenuManager.add(refreshSnippetsAction);
+	}
+	private void createContextMenu(){;}
+	protected void createToolbar(){;}
+	
+	/**
+	 * Gets the root element of the document
+	 * @return the root directory
+	 */
+	public DocItem getRootInput()
+	{
+		try
+		{
+			//get a handle to the current editor and assign it to our temp action
+			IEditorPart iep = this.getViewSite().getWorkbenchWindow().getActivePage().getActiveEditor();
+			//((FileEditorInput)iep.getEditorInput()).getFile();
+			ITextEditor ite = (ITextEditor)iep; //.getEditorInput();
+			ICFDocument icfd = (ICFDocument)ite.getDocumentProvider().getDocument(iep.getEditorInput());
+			
+			//IDocument doc = editor.getDocumentProvider().getDocument(editor.getEditorInput());
+			
+			//icfd.parseDocument();
+			//CFDocument cfd = icfd.getParser().getParseResult();
+			//CFDocument cfd = icfd.getParser().parseDoc();
+			//icfd.getParser().parseSaveDoc();
+			//CFDocument cfd = icfd.getParser().getParseResult();
+			
+			//cfd.docVariables
+			
+			ArrayList tmitems = icfd.getParser().getTagMatches(icfd);
+			CFDocument cfd = icfd.getParser().createDocTree(tmitems);
+			
+			//CFDocument cfd = icfd.getParser().parseDoc(icfd);
+			System.out.println("**********" + cfd.getFilename() + "********");
+			
+			Stack items = cfd.docTree;
+			
+			if(items == null || items.size() < 1){
+				System.err.println("No items?!");
+			}
+			else
+			{
+				//DocItem docroot = (DocItem)items.get(0);
+				//TagMatch tm = (TagMatch)items.get(0);
+				
+				//System.out.println(
+				//	"got " + tm.match
+				//);
+				
+				java.util.Iterator i = items.iterator();
+				while(i.hasNext())
+				{
+					System.err.println("Item::::" + (DocItem)i.next());
+				}
+				
+				//return tm;
+				return (DocItem)items.get(0);
+			}
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace(System.err);
+		}
+		
+		return null; //new TagMatch("/",0,0,1);
+	}
+	
+	public void setFocus(){;}
+}
