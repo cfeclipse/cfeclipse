3ab4c66ddb16e7c414c123aa7675d8fb4195f7f1
Rob Rohan <me@robrohan.com>
2004-07-21 08:01:48 +0000

    Trying to get the preference pages usable... they still are not but getting better...

---
+++ cvs/trunk/src/version1/src/com/rohanclan/cfml/preferences/CFMLColorsPreferencePage.java
@@ -0,0 +1,77 @@
+/*
+ * Created on Jun 17, 2004
+ *
+ * The MIT License
+ * Copyright (c) 2004 Rob Rohan
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a 
+ * copy of this software and associated documentation files (the "Software"), 
+ * to deal in the Software without restriction, including without limitation 
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+ * and/or sell copies of the Software, and to permit persons to whom the Software 
+ * is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in 
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, 
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE 
+ * SOFTWARE.
+ */
+package com.rohanclan.cfml.preferences;
+
+//import org.eclipse.jface.preference.BooleanFieldEditor;
+import org.eclipse.jface.preference.ColorFieldEditor;
+import org.eclipse.jface.preference.FieldEditorPreferencePage;
+//import org.eclipse.jface.preference.IPreferenceStore;
+import org.eclipse.ui.IWorkbenchPreferencePage;
+//import org.eclipse.jface.preference.*;
+//import org.eclipse.ui.IWorkbenchPreferencePage;
+import org.eclipse.ui.IWorkbench;
+import com.rohanclan.cfml.CFMLPlugin;
+//import com.rohanclan.cfml.editors.ICFColorConstants;
+
+/**
+ * @author Rob
+ *
+ * TODO To change the template for this generated type comment go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+public class CFMLColorsPreferencePage extends FieldEditorPreferencePage implements 
+	IWorkbenchPreferencePage, ICFMLPreferenceConstants {
+	
+	public CFMLColorsPreferencePage() 
+	{
+		super(GRID);
+		setPreferenceStore(CFMLPlugin.getDefault().getPreferenceStore());
+		setDescription("On this page you can change the colour preferences for CFML sections.");
+	}
+	
+	/**
+	 * Creates the field editors. Field editors are abstractions of
+	 * the common GUI blocks needed to manipulate various types
+	 * of preferences. Each field editor knows how to save and
+	 * restore itself.
+	 */
+	public void createFieldEditors()
+	{
+		addField(new ColorFieldEditor(P_COLOR_DEFAULT_TEXT,"Default &Text:",getFieldEditorParent()));	
+		addField(new ColorFieldEditor(P_COLOR_CFCOMMENT,"CFML &Comment:",getFieldEditorParent()));
+		addField(new ColorFieldEditor(P_COLOR_CFTAG,"CFML &Tag:",getFieldEditorParent()));
+		addField(new ColorFieldEditor(P_COLOR_CFSTRING,"CFML St&ring:",getFieldEditorParent()));
+		addField(new ColorFieldEditor(P_COLOR_CFKEYWORD,"CFML &Keyword:",getFieldEditorParent()));
+		addField(new ColorFieldEditor(P_COLOR_CFNUMBER,"CFML &Number:",getFieldEditorParent()));
+		
+		
+		addField(new ColorFieldEditor(P_COLOR_CFSCRIPT_TEXT,"CFScript Te&xt:",getFieldEditorParent()));
+		addField(new ColorFieldEditor(P_COLOR_CFSCRIPT_KEYWORD,"CFScript Key&word:",getFieldEditorParent()));
+		addField(new ColorFieldEditor(P_COLOR_CFSCRIPT_FUNCTION,"CFScript Funct&ion:",getFieldEditorParent()));
+		addField(new ColorFieldEditor(P_COLOR_CFSCRIPT_STRING,"CFScript Strin&g:",getFieldEditorParent()));
+	}
+	 
+	public void init(IWorkbench workbench){;}
+}
