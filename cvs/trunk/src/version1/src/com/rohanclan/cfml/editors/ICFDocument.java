bc0194b2736334220dc6620829c2cdd48470ebc7
Oliver Tupman <olivert.tupman@gmail.com>
2004-10-13 22:29:06 +0000

    - IDocument now keeps a cache of the gathered CAM it is using - Cached CAM is generated from checking for natures that implement IContentAssistContributingNature and getting their CAMs - Cached CAM updated whenever a contributing nature changes it's own CAM - Note to self: Should CAM refresh date/time live in the CAM itself? - NB: The behaviour of IDocument when CFE itself isn't associated with the containing project is UNDEFINED! - Note to self: Need to check for CFE nature. If it does not exist then it should get the global CAM.

--- cvs/trunk/src/version1/src/com/rohanclan/cfml/editors/ICFDocument.java
+++ cvs/trunk/src/version1/src/com/rohanclan/cfml/editors/ICFDocument.java
@@ -28,18 +28,29 @@ package com.rohanclan.cfml.editors;
 
 //import java.util.Iterator;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Iterator;
+
 import com.rohanclan.cfml.CFMLPlugin;
 import com.rohanclan.cfml.editors.contentassist.CFEContentAssistManager;
+import com.rohanclan.cfml.editors.contentassist.IAssistAttrValueContributor;
+import com.rohanclan.cfml.editors.contentassist.IAssistContributor;
+import com.rohanclan.cfml.editors.contentassist.IAssistTagContributor;
+import com.rohanclan.cfml.natures.CFENature;
+import com.rohanclan.cfml.natures.IContentAssistContributorNature;
 import com.rohanclan.cfml.parser.CFDocument;
 import com.rohanclan.cfml.parser.CFNodeList;
 import com.rohanclan.cfml.parser.CFParser;
 import com.rohanclan.cfml.parser.CommentParser;
 import com.rohanclan.cfml.parser.docitems.CfmlTagItem;
 import com.rohanclan.cfml.preferences.ICFMLPreferenceConstants;
+import com.rohanclan.cfml.util.ResourceUtils;
 //import com.rohanclan.cfml.parser.DocItem;
 
 import org.eclipse.core.resources.IMarker;
 import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IProjectNature;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProduct;
@@ -47,6 +58,7 @@ import org.eclipse.core.runtime.QualifiedName;
 //import org.eclipse.jface.text.IDocument; 
 import org.eclipse.jface.preference.IPreferenceStore;
 import org.eclipse.jface.text.Document;
+import org.eclipse.jface.text.contentassist.IContentAssistProcessor;
 
 //import org.eclipse.ui.IEditorPart;
 
@@ -84,9 +96,20 @@ public class ICFDocument extends Document implements ICFEFileDocument {
 	/** The parse result of the document. NB: can be null if there were errors */
 	private CFDocument docStructure = null;
 	
+	/**
+     * The private CAM for this document.
+     * NB: Not used as it's a possible solution for per-project frameworks
+     * (and therefore per-project CACors)
+     */
+    private CFEContentAssistManager docCAM;
+	
+    /** Represents the time at which the CAM was last refreshed */
+    private Date lastCAMRefresh;
+    
 	public ICFDocument()
 	{
 		super();
+		this.docCAM = null;
 	}
 	
 	/**
@@ -176,8 +199,7 @@ public class ICFDocument extends Document implements ICFEFileDocument {
 	 */
 	public void setParserResource(IResource newRes)
 	{
-		lastRes = newRes;
-		
+	    this.lastRes = newRes;
 		if(docParser == null)
 		{
 			docParser = new CFParser(this, lastRes);
@@ -254,14 +276,108 @@ public class ICFDocument extends Document implements ICFEFileDocument {
 	 * @see com.rohanclan.cfml.editors.ICFEFileDocument
 	 */
     public CFEContentAssistManager getContentAssistManager() {
-        return CFMLPlugin.getDefault().getGlobalCAM();
+		if(this.lastRes == null)
+		    return CFMLPlugin.getDefault().getGlobalCAM();
+
+		CFEContentAssistManager returnManager = null;
+		CFENature nature;		    
+		try {
+		    if(!CFENature.hasCFENature(this.lastRes.getProject()))
+		        return CFMLPlugin.getDefault().getGlobalCAM();
+		    
+		    nature = (CFENature)lastRes.getProject().getNature(CFMLPlugin.NATURE_ID);
+		    //returnManager = nature.getNatureCAM();
+		    returnManager = getCAM();
+		} catch(CoreException ex) {
+		    ex.printStackTrace();
+		}
+		return returnManager;
     }
+    
+    private CFEContentAssistManager getCAM()
+    {
+        try {
+            refreshCAM();
+        }
+        catch(CoreException ex)
+        {
+            ex.printStackTrace();
+        }
+        return this.docCAM;
+    }
+    
     /**
-     * The private CAM for this document.
-     * NB: Not used as it's a possible solution for per-project frameworks
-     * (and therefore per-project CACors)
+     * Recreates the document's CAM based upon the natures that provide content
+     * assist.
+     * 
+     * @param natureIter Iterator of a collection of IContentAssistContributorNature's
      */
-    //private CFEContentAssistManager docCAM;
+    private void refreshCAMFromList(Iterator natureIter)
+    {
+        this.docCAM = new CFEContentAssistManager();
+        while(natureIter.hasNext())
+        {
+            CFEContentAssistManager currManager = ((IContentAssistContributorNature)natureIter.next()).getNatureCAM();
+            Iterator currIter = currManager.getRootAssistors().iterator();
+            while(currIter.hasNext())
+            {
+                Object currRoot = currIter.next();
+                if(currRoot instanceof IAssistContributor)
+                    this.docCAM.registerRootAssist((IAssistContributor)currRoot);
+                else if(currRoot instanceof IContentAssistProcessor)
+                    this.docCAM.registerRootAssist((IContentAssistProcessor)currRoot);
+            }
+            
+            currIter = currManager.getTagAssistors().iterator();
+            while(currIter.hasNext())
+            {
+                this.docCAM.registerTagAssist((IAssistContributor)currIter.next());
+            }
+            
+            currIter = currManager.getAttributeAssistors().iterator();
+            while(currIter.hasNext())
+                this.docCAM.registerAttributeAssist((IAssistTagContributor)currIter.next());
+            
+            currIter = currManager.getValueAssistors().iterator();
+            while(currIter.hasNext())
+                this.docCAM.registerValueAssist((IAssistAttrValueContributor)currIter.next());
+        }
+    }
+    
+    private void refreshCAM() throws CoreException
+    {
+        if(this.docCAM == null)
+        {
+            this.docCAM = new CFEContentAssistManager();
+            this.lastCAMRefresh = new Date(0);	// Init last refresh date to the epoch
+        }
+        IProject project = this.lastRes.getProject();
+        IProjectNature [] natures = ResourceUtils.getProjectNatures(project);
+        ArrayList cacNatures = new ArrayList();
+        
+        boolean needUpdate = false;
+        
+        for(int i = 0; i < natures.length; i++)
+        {
+            if(!(natures[i] instanceof IContentAssistContributorNature))
+                continue;
+            
+            IContentAssistContributorNature cacorNature = (IContentAssistContributorNature)natures[i];
+            
+            if(cacorNature.getLastUpdateDate().after(this.lastCAMRefresh))
+            {
+                needUpdate = true;
+            }
+            cacNatures.add(cacorNature);
+        }
+        
+        if(needUpdate)
+        {
+            refreshCAMFromList(cacNatures.iterator());
+        }
+        
+        this.lastCAMRefresh = new Date();
+    }
     
     /**
      * Refreshes this document's CAM from the framework natures associated
