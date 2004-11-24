b12b7dc4b77276305d7a792582403b4cc203a16b
Oliver Tupman <olivert.tupman@gmail.com>
2004-11-24 17:37:54 +0000

    - Just doing some basic commenting of some classes 'cos I need to look like I'm doing something.

--- cvs/trunk/src/version1/src/com/rohanclan/cfml/util/AlertUtils.java
+++ cvs/trunk/src/version1/src/com/rohanclan/cfml/util/AlertUtils.java
@@ -1,11 +1,28 @@
 /*
  * Created on Nov 10, 2004
  *
- * TODO To change the template for this generated file go to
- * Window - Preferences - Java - Code Generation - Code and Comments
+ * The MIT License
+ * Copyright (c) 2004 Stephen Milligan
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a 
+ * copy of this software and associated documentation files (the "Software"), 
+ * to deal in the Software without restriction, including without limitation 
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+ * and/or sell copies of the Software, and to permit persons to whom the Software 
+ * is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in 
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, 
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE 
+ * SOFTWARE.
  */
 package com.rohanclan.cfml.util;
-
 
 import org.eclipse.swt.widgets.MessageBox;
 import org.eclipse.swt.widgets.Display;
@@ -17,10 +34,12 @@ import org.eclipse.ui.*;
 import org.eclipse.jface.action.IStatusLineManager;
 
 /**
+ * A series of helper functions to make life easier for us to tell
+ * the user about anything that we feel like. Helper functions are
+ * available for message boxes and for displaying information on
+ * the status bar.
+ * 
  * @author Stephen Milligan
- *
- * TODO To change the template for this generated type comment go to
- * Window - Preferences - Java - Code Generation - Code and Comments
  */
 public class AlertUtils {
 
@@ -32,17 +51,33 @@ public class AlertUtils {
         // TODO Auto-generated constructor stub
     }
     
+    /**
+     * Alerts the user to an exception. No formatting is applied to the incoming
+     * exception so what you give is what the user gets.
+     * 
+     * This method also logs the exception to the console in case a developer is
+     * a-watching and wishes to view the log. 
+     * 
+     * @param e The exception to display to the user.
+     */
     public static void alertUser(Exception e) {
         MessageBox msg = new MessageBox(Display.getCurrent().getActiveShell(),SWT.ICON_ERROR);
         String message = e.getMessage();
         if (e.getMessage() == null) {
             message = e.toString();
         }
+        e.printStackTrace();
         msg.setText("Error!");
         msg.setMessage(message);
         msg.open();
     }
     
+    /**
+     * Alerts the user as to an error message.
+     * The message will be displayed with the title "Error!".
+     * 
+     * @param message The message to display.
+     */
     public static void alertUser(String message) {
         MessageBox msg = new MessageBox(Display.getCurrent().getActiveShell(),SWT.ICON_INFORMATION);
         msg.setText("Error!");
@@ -50,6 +85,12 @@ public class AlertUtils {
         msg.open();
     }
     
+    /**
+     * Sets the status bar's message for the user for an input. 
+     * 
+     * @param message The message to display
+     * @param editorPart The editor part that wishes to display the message.
+     */
     public static void showStatusMessage(String message,IEditorPart editorPart) {
         try {
          IStatusLineManager statusManager = editorPart.getEditorSite().getActionBars().getStatusLineManager();
@@ -60,6 +101,12 @@ public class AlertUtils {
         }
     }
     
+    /**
+     * Sets an error status bar message for the user for an input. 
+     * 
+     * @param message The message to display
+     * @param viewPart The view part that wishes to display the message
+     */
     public static void showStatusErrorMessage(String message,IViewPart viewPart) {
         try {
          IStatusLineManager statusManager = viewPart.getViewSite().getActionBars().getStatusLineManager();
@@ -70,6 +117,12 @@ public class AlertUtils {
         }
     }
     
+    /**
+     * Sets the status bar's message for the user for an input. 
+     * 
+     * @param message The message to display
+     * @param viewPart The view part that wishes to display the message.
+     */
     public static void showStatusMessage(String message,IViewPart viewPart) {
         try {
          IStatusLineManager statusManager = viewPart.getViewSite().getActionBars().getStatusLineManager();
