9341691feefc73b65a4f2b2d4e56389dec73c83d
Mark Drew <mark.drew@gmail.com>
2006-09-16 20:20:49 +0000

    Fixes Ticket #4 and the reading of false positive and false negative attributes in tags like cffile and cfdirectory

--- svn/branches/1.3-dev/src/org/cfeclipse/cfml/parser/docitems/CfmlTagItem.java
+++ svn/branches/1.3-dev/src/org/cfeclipse/cfml/parser/docitems/CfmlTagItem.java
@@ -29,10 +29,9 @@ package org.cfeclipse.cfml.parser.docitems;
 //import java.util.HashMap;
 //import java.util.Map;
 //import java.util.Iterator;
-import java.util.Currency;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Set;
-import java.util.Map.Entry;
 
 import org.cfeclipse.cfml.dictionary.Parameter;
 import org.cfeclipse.cfml.dictionary.Tag;
@@ -49,7 +48,121 @@ public class CfmlTagItem extends TagItem {
 	 * @see org.cfeclipse.cfml.parser.docitems.DocItem#IsSane()
 	 */
 	public boolean IsSane() {
+		//staticLookups
+
+		
+		HashMap suggestedAttributes = new HashMap();
+		
+		Set suggAttribSet = itemAttributes.keySet();
+		
+		for (Iterator iter = suggAttribSet.iterator(); iter.hasNext();) {
+			String attributeName = (String) iter.next();
+			AttributeItem attributeValue = (AttributeItem)itemAttributes.get(attributeName);
+			suggestedAttributes.put(attributeName, attributeValue.getValue());
+			
+		}
+		
+		
+		//this.itemAttributes; //Who is setting this? it is badly set, lets create our own hashmap
+		
+	
 		Set attributes = syntax.getElementAttributes(this.itemName);
+
+		//Check to find all required attributes.
+		Object[] params = attributes.toArray();
+		for(int i = 0; i < params.length; i++)
+		{
+			Parameter currParam = (Parameter)params[i];
+			
+			if(currParam.isRequired() && !itemAttributes.containsKey(currParam.getName()))
+			{
+					this.parseMessages.addMessage(new ParseError(lineNumber, startPosition, endPosition, itemData,
+						 "The attribute \'" + currParam.getName() + "\' is compulsory for the <cf" + itemName + "> tag."));
+			}
+			
+			if(!currParam.getTriggers().isEmpty()  && currParam.isRequired(suggestedAttributes) == 3 && !itemAttributes.containsKey(currParam.getName())){
+				
+				this.parseMessages.addMessage(new ParseError(lineNumber, startPosition, endPosition, itemData,
+						"The attribute \'" + currParam.getName() + "\' is required for the <cf" + itemName + "> tag."));
+			}
+			else if (!currParam.getTriggers().isEmpty()  && currParam.isTriggered(suggestedAttributes) == 0 && itemAttributes.containsKey(currParam.getName())) {
+				
+				this.parseMessages.addMessage(new ParseError(lineNumber, startPosition, endPosition, itemData,
+						"The attribute \'" + currParam.getName() + "\' is not valid for the <cf" + itemName + "> tag."));
+			}
+			//now check for items that shouldnt be there, i.e. are NOT triggered
+			
+		}
+		
+		
+		
+		//Check 2: check to see if the suggested attributes have been triggered.
+		
+		//Now I have checked for all the required attributes, lets check if any of the suggested attributes have triggers, 
+		//if they have a trigger, see if its amongst the ones I have written
+		
+		
+		
+		return super.IsSane();
+		
+		
+		
+		//quick check if it requires anything, and if we have put stuff in there
+		/*
+		if(definedAttributes == null){
+			return super.IsSane();
+		}
+		
+		for (Iterator iter = definedAttributes.iterator(); iter.hasNext();) {
+			Parameter element = (Parameter) iter.next();
+			System.out.println(element.getName());
+				System.out.println("\t required " + element.isRequired(suggestedAttributes));
+				System.out.println("\t triggered " + element.isTriggered(suggestedAttributes) +" " + Parameter.PARAM_TRIGGERED);
+				
+				//	if(element.isRequired(suggestedAttributes)){
+					//	this.parseMessages.addMessage(new ParseError(lineNumber, startPosition, endPosition, itemData,
+					//			 "The attribute \'" + element.getName() + "\' is compulsory for the <" + itemName + "> tag."));
+						
+				//	}
+				
+				if(element.isRequired()){
+					switch (element.isTriggered(suggestedAttributes)) {
+					case Parameter.PARAM_NOTTRIGGERED:
+						//Not triggered, but is it required?
+						
+						//	System.out.println("\tnot triggered");						
+							break;
+					case Parameter.PARAM_TRIGGERED:
+						//System.out.println("\ttriggered");						
+						break;
+
+					case Parameter.PARAM_REQUIRED:
+						//System.out.println("\trequired");
+						break;
+						
+					default:
+						break;
+					}
+				}
+				else{
+					//System.out.println("\tnot required");
+				}
+		}
+		
+		
+*/		
+		//As we loop through the definedAttributes, we can check if we have the required ones, 
+		//if one is required, then we check if there are selected value requirements
+		
+		
+		
+		
+		
+		
+		
+		//Need to change this, so that if there is a missing item it reports an error.
+	
+		
 		/*	//TODO: Warning This keeps throwing a null pointer exception with cffile, cfdirectory and cflocation. 
 		
 		if (attributes == null) {
@@ -74,7 +187,7 @@ public class CfmlTagItem extends TagItem {
 		}
 		return super.IsSane();*/
 		
-		
+		/*
 		if (attributes == null) {
 			return super.IsSane();
 		}
@@ -85,24 +198,24 @@ public class CfmlTagItem extends TagItem {
 			
 			Parameter currParam = (Parameter)params[i];
 	
-			/*if(currParam.isReqpuired(itemAttributes)){
+			if(currParam.isReqpuired(itemAttributes)){
 			 * 
 			 * (currParam.isTriggered(itemAttributes) & Parameter.PARAM_REQUIRED) == Parameter.PARAM_REQUIRED) && 
 					   
 				this.parseMessages.addMessage(new ParseError(lineNumber, startPosition, endPosition, itemData,
 						 "The attribute \'" + currParam.getName() + "\' is compulsory for the <" + itemName + "> tag."));
 			}
-			*/
+			
 			//some items that shouldnt be required.. need to see if it has been triggered?
-			System.out.println("ATTR: " + currParam.getName() + " required " + currParam.isReqpuired(itemAttributes) + " triggered "  + (currParam.isTriggered(itemAttributes) & Parameter.PARAM_REQUIRED));
+			//System.out.println("ATTR: " + currParam.getName() + " required " + currParam.isReqpuired(itemAttributes) + " triggered "  + (currParam.isTriggered(itemAttributes) & Parameter.PARAM_REQUIRED));
 			
 		   if( currParam.isReqpuired(itemAttributes) & !itemAttributes.containsKey(currParam.getName()))
 			{
 				this.parseMessages.addMessage(new ParseError(lineNumber, startPosition, endPosition, itemData,
 						 "The attribute \'" + currParam.getName() + "\' is compulsory for the <" + itemName + "> tag."));
 			}
-		}
-		return super.IsSane();
+		}*/
+		//return super.IsSane();
 	}
 	/**
 	 * Determines whether the child is valid or not.
