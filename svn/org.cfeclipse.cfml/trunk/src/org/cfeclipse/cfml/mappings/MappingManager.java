99d0c2f9410f8cc37316b9875b04c96b310a8a76
Mike Kelp <mike.kelp@gmail.com>
2007-04-27 15:07:43 +0000

    - Commented - Filled in setMapping()

--- svn/org.cfeclipse.cfml/trunk/src/org/cfeclipse/cfml/mappings/MappingManager.java
+++ svn/org.cfeclipse.cfml/trunk/src/org/cfeclipse/cfml/mappings/MappingManager.java
@@ -1,10 +1,9 @@
-/**
- * 
- */
 package org.cfeclipse.cfml.mappings;
 
 import java.util.ArrayList;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.eclipse.core.resources.IFolder;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IResource;
@@ -15,21 +14,45 @@ import org.eclipse.core.runtime.QualifiedName;
  * This statuic class manages the the figuring out and setting of mappings for
  * IProjects and IFolders 
  * @author markdrew
- *
  */
 public class MappingManager {
 
+	// Logger for this class
+	private Log logger = LogFactory.getLog(MappingManager.class);
+
 	
-	
-	public static void setMapping(IResource resource, String mapping){
+	/**
+	 * Sets a mapping to a resource so that it can be used in path translation.
+	 * @param resource The resource the mapping references.
+	 * @param mapping  The mapping name used to reference the resource.
+	 * @author Mike Kelp  
+	 */
+	public static void setMapping(IResource resource, String mapping) {
+		// Ensure we have a / at the beginning of the mapping for consistency
+		if (mapping.charAt(0) != '/') {
+			mapping = "/".concat(mapping);
+		}
 		
+		// Set the mapping property of this resource
+		try {
+			resource.setPersistentProperty(new QualifiedName("org.cfeclipse.cfml", "Mapping"), mapping);
+		} catch (CoreException e) {
+			e.printStackTrace();
+		}
 	}
+
 	
+	/**
+	 * Gets the mappings set for a given project.
+	 * @param project The project to search for mappings.
+	 * @return An array of mappings that exist for the project.
+	 * @author Mark Drew
+	 */
 	public CFMapping[] getMappings(IProject project){
 		ArrayList mappings = new ArrayList();
 		findMappings(project, mappings);
 		
-		//if they are null, we set the project as having the base "/" mapping
+		// if they are null, we set the project as having the base "/" mapping
 		if(mappings.isEmpty()){
 			setMapping(project, "/");
 			CFMapping mapping = new CFMapping(project, "/");
@@ -38,9 +61,15 @@ public class MappingManager {
 		
 		return (CFMapping[]) mappings.toArray(new CFMapping[mappings.size()]);
 	}
-	
+
+
+	/**
+	 * Recursively searches a resource for mappings and appends them to the mappings ArrayList.
+	 * @param resource The resource to search for mappings.
+	 * @param mappings The ArrayList to add the mappings to.
+	 * @author Mark Drew
+	 */
 	private void findMappings(IResource resource, ArrayList mappings){
-		
 		if (resource instanceof IProject) {
 			IProject mappingCandidate = (IProject) resource;
 			String mapping = getMapping(mappingCandidate);
@@ -75,23 +104,25 @@ public class MappingManager {
 				}
 			}
 		}
-		
-		
 	}
+
 	
+	/**
+	 * Gets the mapping property for a resource.
+	 * @param resource
+	 * @return
+	 * @author Mark Drew
+	 */
 	public static String getMapping(IResource resource){
-		
-			String persistentProperty = null;
-			try {
-				persistentProperty = resource.getPersistentProperty(new QualifiedName("org.cfeclipse.cfml", "Mapping"));
-			} catch (CoreException e) {
-				e.printStackTrace();
-			}
-			if(persistentProperty != null && persistentProperty.length() > 0){
-				return persistentProperty;
-			}
-		
-		return persistentProperty;
+		String persistentProperty = null;
+
+		try {
+			persistentProperty = resource.getPersistentProperty(new QualifiedName("org.cfeclipse.cfml", "Mapping"));
+		} catch (CoreException e) {
+			e.printStackTrace();
+		}
+
+		return (persistentProperty == null) ? "" : persistentProperty;
 	}
 	
 }
