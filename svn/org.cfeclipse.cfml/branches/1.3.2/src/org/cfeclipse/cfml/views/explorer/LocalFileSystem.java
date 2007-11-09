d6751dfbe92c5d8d0cd7111ad64304e44d25a944
Mark Drew <mark.drew@gmail.com>
2007-11-09 07:36:41 +0000

    fixes #332

--- svn/org.cfeclipse.cfml/branches/1.3.2/src/org/cfeclipse/cfml/views/explorer/LocalFileSystem.java
+++ svn/org.cfeclipse.cfml/branches/1.3.2/src/org/cfeclipse/cfml/views/explorer/LocalFileSystem.java
@@ -79,7 +79,7 @@ public class LocalFileSystem implements IFileProvider
 			{
 				String driveLetter = systemroot[i].toString();
 				FileSystemRoot drive = new FileSystemRoot(driveLetter);
-				drive.setType("file");
+				//drive.setType("file");
 				drive.setPath(systemroot[i].toString());
 				tmpRoots.add(drive);
 			}
