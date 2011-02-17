0b4f6b756980b8571582fb514d3a9cdc77033884
Denny Valliant <valliantster@gmail.com>
2011-02-17 07:28:59 +0000

    CLOSED - # 618: 'Suggest templates' listed twice on Auto-insertion and closing menu http://trac.cfeclipse.org/ticket/618

--- svn/org.cfeclipse.cfml/trunk/src/org/cfeclipse/cfml/preferences/AutoIndentPreferencePage.java
+++ svn/org.cfeclipse.cfml/trunk/src/org/cfeclipse/cfml/preferences/AutoIndentPreferencePage.java
@@ -55,7 +55,6 @@ public class AutoIndentPreferencePage
 		addField(new BooleanFieldEditor(AutoIndentPreferenceConstants.P_SUGGEST_VARIABLES, "Suggest Variables", getFieldEditorParent()));
 		addField(new BooleanFieldEditor(AutoIndentPreferenceConstants.P_SUGGEST_HTML, "Suggest HMTL Tags", getFieldEditorParent()));
 		addField(new BooleanFieldEditor(AutoIndentPreferenceConstants.P_SUGGEST_TEMPLATES, "Suggest Templates", getFieldEditorParent()));
-		addField(new BooleanFieldEditor(AutoIndentPreferenceConstants.P_SUGGEST_TEMPLATES, "Suggest Templates", getFieldEditorParent()));
 		
 		}
 		catch (Exception e){
