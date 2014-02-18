1363995b381bb39b63f6f175dbadc6d011d7806e
denuno <valliantster@gmail.com>
2014-02-18 11:16:15 -0700

    remove docshare34 plugin and refactored docshare35 to be docshare

--- org.cfeclipse.cfml.docsharesupport/src/org/cfeclipse/cfml/docsharesupport/Activator.java
+++ org.cfeclipse.cfml.docsharesupport/src/org/cfeclipse/cfml/docsharesupport/Activator.java
@@ -1,4 +1,4 @@
-package org.cfeclipse.cfml.docsharesupport35;
+package org.cfeclipse.cfml.docsharesupport;
 
 import org.eclipse.ui.plugin.AbstractUIPlugin;
 import org.osgi.framework.BundleContext;
@@ -9,7 +9,7 @@ import org.osgi.framework.BundleContext;
 public class Activator extends AbstractUIPlugin {
 
 	// The plug-in ID
-	public static final String PLUGIN_ID = "org.cfeclipse.cfml.docsharesupport35";
+	public static final String PLUGIN_ID = "org.cfeclipse.cfml.docsharesupport";
 
 	// The shared instance
 	private static Activator plugin;
